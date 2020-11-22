99a53eb66d7f6810482ed316bb5eb72923f9604b
Ander Swift <anderswift@gmail.com>
2020-11-21 22:43:10 -0500

    feat: import api; create variables for user data

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,3 +1,7 @@
+
+import {useState} from 'react';
+
+import api from '../utils/api.js';
 import avatar from '../images/avatar.png';
 
 function Main(props) {
@@ -5,6 +9,9 @@ function Main(props) {
   const handleEditProfileClick= props.onEditProfile; 
   const handleAddPlaceClick= props.onAddPlace; 
 
+  const [userName, setUserName]= useState('');
+  const [userAbout, setUserAbout]= useState('');
+  const [userAvatar, setUserAvatar]= useState(avatar);
 
   return (
 
@@ -12,12 +19,12 @@ function Main(props) {
           
       <section className="profile">
         <button className="profile__edit-avatar" aria-label="Edit Avatar" onClick={handleEditAvatarClick}>
-          <img className="profile__avatar" src={avatar} alt="Avatar" />
+          <img className="profile__avatar" src={userAvatar} alt="Avatar" />
         </button>
         <div className="profile__info">
-          <h1 className="profile__name"></h1>
+          <h1 className="profile__name">{userName}</h1>
           <button type="button" className="profile__edit-info button" aria-label="Edit profile" onClick={handleEditProfileClick}></button>
-          <p className="profile__about"></p>
+          <p className="profile__about">{userAbout}</p>
         </div>
         
         <button type="button" className="profile__add-image button" aria-label="Add image" onClick={handleAddPlaceClick}></button>
